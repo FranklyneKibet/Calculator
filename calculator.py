5e95c80e29630ba63cc429f7144da2c48ff49835
FranklyneKibet <bettfranklyne@gmail.com>
2021-04-16 16:47:45 +0300

    changes on layout

--- calculator.py
+++ calculator.py
@@ -7,13 +7,16 @@ root = Tk()
 root.title('Calculator')
 
 #Adding input field
-display = Entry(root, borderwidth=5)
-display.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+display = Entry(root, borderwidth=5, width=90)
+display.grid(row=0, column=0, columnspan=6, padx=10, pady=15)
 
 #Defining functions
-def get_variables():  
-    return
-    
+i=0
+def get_variables(num):
+    global i  
+    display.insert(i,num)
+    i +=1   
+     
 #Adding Buttons 
 Button(root, text='1', padx=40,pady=20,command=lambda: get_variables(1)).grid(row=4, column=2)
 Button(root, text='2', padx=40,pady=20,command=lambda: get_variables(2)).grid(row=4, column=3)
@@ -27,29 +30,29 @@ Button(root, text='9', padx=40,pady=20,command=lambda: get_variables(9)).grid(ro
 Button(root, text='0', padx=40,pady=20,command=lambda: get_variables(0)).grid(row=5, column=2)
 
 #Adding operations button
-Button(root,text='%').grid(row=1,column=4)
-Button(root,text='AC').grid(row=1,column=5)
-Button(root,text='÷').grid(row=2,column=5)
-Button(root,text='x').grid(row=3,column=5)
-Button(root,text='-').grid(row=4,column=5)
-Button(root,text='+').grid(row=5,column=5)
-Button(root,text='=').grid(row=5,column=4)
-Button(root,text='.').grid(row=5,column=3)
-Button(root,text='(').grid(row=1,column=2)
-Button(root,text=')').grid(row=1,column=3)
+Button(root,text='%', padx=38,pady=20).grid(row=1,column=4)
+Button(root,text='AC', padx=35,pady=20).grid(row=1,column=5)
+Button(root,text='÷', padx=38,pady=20).grid(row=2,column=5)
+Button(root,text='x', padx=39,pady=20).grid(row=3,column=5)
+Button(root,text='-', padx=40,pady=20).grid(row=4,column=5)
+Button(root,text='+', padx=38,pady=20).grid(row=5,column=5)
+Button(root,text='=', padx=39,pady=20).grid(row=5,column=4)
+Button(root,text='.', padx=41,pady=20).grid(row=5,column=3)
+Button(root,text='(', padx=41,pady=20).grid(row=1,column=2)
+Button(root,text=')', padx=41,pady=20).grid(row=1,column=3)
 
 #Adding Advanced  Operations
-Button(root,text='Del').grid(row=1,column=0)
-Button(root,text='ln').grid(row=1,column=1)
-Button(root,text='x!').grid(row=2,column=0)
-Button(root,text='sin').grid(row=2,column=1)
-Button(root,text='cos').grid(row=3,column=1)
-Button(root,text='tan').grid(row=4,column=1)
-Button(root,text='π').grid(row=3,column=0)
-Button(root,text='log').grid(row=4,column=0)
-Button(root,text='Exp').grid(row=5,column=0)
-Button(root,text='x^y').grid(row=5,column=1)
+Button(root,text='Del', padx=34,pady=20).grid(row=1,column=0)
+Button(root,text='ln', padx=40,pady=20).grid(row=1,column=1)
+Button(root,text='x!', padx=38,pady=20).grid(row=2,column=0)
+Button(root,text='sin', padx=37.4,pady=20).grid(row=2,column=1)
+Button(root,text='cos', padx=35.6,pady=20).grid(row=3,column=1)
+Button(root,text='tan', padx=36.4,pady=20).grid(row=4,column=1)
+Button(root,text='π', padx=38,pady=20).grid(row=3,column=0)
+Button(root,text='log', padx=33,pady=20).grid(row=4,column=0)
+Button(root,text='Exp', padx=32,pady=20).grid(row=5,column=0)
+Button(root,text='x^y', padx=34.8,pady=20).grid(row=5,column=1)
 
-
+root.resizable(False,False)
 root.mainloop()
 
